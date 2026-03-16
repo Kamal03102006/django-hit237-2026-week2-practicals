0ceebca662b3320f4106b2ee16eafa26e94f4610
Kamal03102006 <kamalpreetth0@gmail.com>
2026-03-16 21:51:13 +0930

    Commit

--- blog_app/views.py
+++ blog_app/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Post
 
-# Create your views here.
+def post_list(request, pk):
+    # Retrieve the specific post by its primary key (pk)
+    # or return a 404 error if it doesn't exist
+    posts = get_object_or_404(Post, pk=pk)
+
+    context = {
+        'posts': posts,
+    }
+
+    return render(request, 'blog_app/post_list.html', context)
