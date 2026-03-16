e77d900df295f4aacfc569e4a81c068d40d8d9b0
Kamal03102006 <kamalpreetth0@gmail.com>
2026-03-16 21:55:49 +0930

    Commit

--- blog_app/urls.py
+++ blog_app/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('<int:pk>/', views.post_list, name='post_list'),
+    path('<int:pk>/', views.post_details, name='post_details'),
 ]
